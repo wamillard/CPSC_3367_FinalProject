1300a3cdcaf7c06cd537d475c9e3048524079260
wamillard <wamillard@yahoo.com>
2021-12-04 14:33:00 -0600

    removed first Activity

--- app/src/main/java/com/juarez_millard/finalproject/MainActivity.java
+++ app/src/main/java/com/juarez_millard/finalproject/MainActivity.java
@@ -1,16 +1,13 @@
 package com.juarez_millard.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
